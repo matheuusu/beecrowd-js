a695ff49542425263f3674e63bd4467049d66b7c
Matheus Silva <devtheus0@gmail.com>
2024-06-13 21:53:07 -0300

    feat(javascript): added the solution for exercise 1001

--- javascript/1001/index.js
+++ javascript/1001/index.js
@@ -1,6 +1,10 @@
+// Import the 'fs' module to read the file
+// Split the input string by newline to get an array of lines
 const input = require("fs").readFileSync("./1001/stdin", "utf8")
 const lines = input.split("\n")
 
+// Destructure the lines array to extract the first two numbers and convert them to float
 const [a, b] = lines.map((number) => parseFloat(number))
 
+// Calculate the sum and print the result
 console.log(`X = ${a + b}`)
